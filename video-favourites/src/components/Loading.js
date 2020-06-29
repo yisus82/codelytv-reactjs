2290c580388d8efeb5df85d78dd43af4a7f7b02c
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2020-06-29 20:12:32 +0200

    Loading message typing effect

--- video-favourites/src/components/Loading.js
+++ video-favourites/src/components/Loading.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Typed from 'react-typed';
 
-const Loading = ({ message = 'Loading...' }) => {
-  return <div className='loader'>{message}</div>;
-};
+const Loading = ({ message = 'Loading...', speed = 30 }) => (
+  <div className='loader'>
+    <Typed strings={[message]} typeSpeed={speed} loop />
+  </div>
+);
 
 Loading.propTypes = {
   message: PropTypes.string.isRequired,
+  speed: PropTypes.number,
 };
 
 export default Loading;
